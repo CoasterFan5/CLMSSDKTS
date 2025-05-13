4ab517130cfa2cbe58e3e656ca447d5028e6ebcc
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-05-13 10:00:36 -0400

    fixes

--- examples/index.ts
+++ examples/index.ts
@@ -1,9 +1,12 @@
 import { CanvasLMS } from "../dist";
-import { User } from "../dist/models/User";
 
 const canvas = new CanvasLMS({
   domain: "https://canvas.jmu.edu/api",
   accessToken: process.env.CANVAS_TOKEN!,
 });
 
-console.log(await canvas.listYourCourses({}));
+console.log(
+  await canvas.removeCourseFromFavorites({
+    id: "2036065",
+  }),
+);
