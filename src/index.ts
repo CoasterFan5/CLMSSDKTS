f8f7f06eab17b110aa3e6e119341d574caeadfbe
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-08-28 15:59:13 -0400

    make download skipping an environment varaible

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { codeGen } from "./codegen";
 import { downloadDocData } from "./download";
 
-let skipDownload = true;
+let skipDownload = process.env.SKIP_DOWNLOAD;
 const main = async () => {
-  if (!skipDownload) {
+  if (!skipDownload || skipDownload.toLowerCase() != "true") {
     await downloadDocData();
   } else {
     console.warn("Skipped Download");
