e6071da976de21b76e6266d7050941da2c2a8ffa
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-09-04 17:01:21 -0400

    update example

--- examples/index.ts
+++ examples/index.ts
@@ -10,16 +10,21 @@ try {
 
   if (courses) {
     for (const c of courses) {
+      console.log(`${c.name} (${c.id})`);
       canvas
         .listAssignmentsAssignments({
           course_id: c.id.toString(),
-          "include[]": ["submission"],
+          "include[]": ["submission", "score_statistics"],
         })
         .then((assignments) => {
           if (assignments) {
             for (const a of assignments) {
-              if (a.submission) {
-                console.log(a.submission);
+              if (
+                !a.submission ||
+                (!a.locked_for_user &&
+                  a.submission.workflow_state == "unsubmitted")
+              ) {
+                console.log(`${c.name} (${c.id}) - ${a.name}`);
               }
             }
           }
